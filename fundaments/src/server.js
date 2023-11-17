de5398d5f5c1d73d83cd4f1e05ee1e0860626ca0
gabriellafaria <gabisouf@gmail.com>
2023-11-17 16:33:43 -0300

    chore: implements the routes

--- fundaments/src/server.js
+++ fundaments/src/server.js
@@ -1,32 +1,19 @@
 import http from 'node:http';
-import { randomUUID } from 'node:crypto';
 import { json } from './middlewares/json.js';
-import { Database } from './database.js';
+import { routes } from './routes.js';
 
-const database = new Database()
 
 const server = http.createServer(async (req, res) => {
   const { method, url } = req
 
   await json(req, res)
 
-  if (method === 'GET' && url === '/users') {
-    const users = database.select('users')
-    return res.end(JSON.stringify(users))
-  }
+  const route = routes.find(route => {
+    return route.method === method && route.path === url
+  })
 
-  if (method === 'POST' && url === '/users') {
-    const { name, email } = req.body
-
-    const users = {
-      id: randomUUID(),
-      name,
-      email,
-    }
-
-    database.insert('users', users)
-
-    return res.writeHead(201).end()
+  if (route) {
+    return route.handler(req, res)
   }
 
   return res.writeHead(404).end()
