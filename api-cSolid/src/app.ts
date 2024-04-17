df0412793b2f61941c843290c5a1e763a28f6ee9
Gabriella Faria <gabisouf@gmail.com>
2024-04-17 16:15:15 -0300

    feat: implements the refreshToken

--- api-cSolid/src/app.ts
+++ api-cSolid/src/app.ts
@@ -1,6 +1,7 @@
 import { ZodError } from 'zod'
 import fastify from 'fastify'
 import { env } from './env'
+import fastifyCookie from '@fastify/cookie'
 
 import fastifyJwt from '@fastify/jwt'
 
@@ -12,7 +13,16 @@ export const app = fastify()
 
 app.register(fastifyJwt, {
   secret: env.JWT_SECRET,
+  cookie: {
+    cookieName: 'refreshToken',
+    signed: false,
+  },
+  sign: {
+    expiresIn: '10m',
+  },
 })
+
+app.register(fastifyCookie)
 
 app.register(usersRoutes)
 app.register(gymsRoutes)
